3b0951ea4c6b6373ec9143ab7655fe3eaea15107
guowei <707634010@qq.com>
2020-12-03 21:21:31 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+// import _ from 'lodash';
+// function component() {
+//     var element = document.createElement('div');
+  
+//     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  
+//     return element;
+//   }
+  
+//   document.body.appendChild(component());
+
+
+// 运行指令
+// webpack ./src/index.js -o ./build/main.js --mode=development
+// import './assets/css/base.css'
+import './base.css'
+import './common.less'
+function add(x,y){
+  return x+y;
+}
+console.log(add(2,8))
